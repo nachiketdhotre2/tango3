ec788f196d6ed8307072bd87a1e4ed046ac23289
nachiket <nachiket.dhotre3@gmail.com>
2017-08-04 12:00:40 +0530

    second commit - tango - database models and postgres

--- tango/settings.py
+++ tango/settings.py
@@ -84,8 +84,14 @@ WSGI_APPLICATION = 'tango.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        #'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': 'db2',
+        'USER': 'postgres',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '5433'
     }
 }
 
