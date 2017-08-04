ed1c604465d1c0ffe159a4d2e3571686f0b5f0ac
nachiket <nachiket.dhotre3@gmail.com>
2017-08-04 16:28:42 +0530

    third commit - tango - model view template

--- rango/views.py
+++ rango/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rango.models import Category
 
 # Create your views here.
 def index(request):
-    context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
+    #context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
+
+    # Query the database for a list of ALL categories currently stored.
+    # Order the categories by no. likes in descending order.
+    # Retrieve the top 5 only - or all if less than 5.
+    # Place the list in our context_dict dictionary
+    # that will be passed to the template engine.
+    category_list = Category.objects.all()
+    context_dict = {'categories': category_list}
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
